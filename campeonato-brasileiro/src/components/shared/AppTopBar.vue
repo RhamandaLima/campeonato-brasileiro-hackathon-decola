a371125a29b4c0e230c4cf475ce4f61d7a3a65ec
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-03-16 15:48:40 -0300

    Add router-link

--- campeonato-brasileiro/src/components/shared/AppTopBar.vue
+++ campeonato-brasileiro/src/components/shared/AppTopBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="app-top-bar">
         <v-app-bar app color="green lighten-1" dark>
             <v-app-bar-nav-icon  @click="drawer = true"></v-app-bar-nav-icon>
             <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
@@ -16,22 +16,24 @@
                 <v-list-item-group
                     v-model="group"
                     active-class="green--text text--lighten-2"
-                >
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-home</v-icon>
-                        </v-list-item-icon>
-                        
-                        <v-list-item-title>Início</v-list-item-title>
-                    </v-list-item>
+                >   <router-link to="/">
+                        <v-list-item>
+                            <v-list-item-icon>
+                                <v-icon>mdi-home</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>Início</v-list-item-title>
+                        </v-list-item>
+                    </router-link>
+                  
+                    <router-link to="/classificacao">
+                        <v-list-item>
+                            <v-list-item-icon>
+                                <v-icon>mdi-soccer</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>Classificação</v-list-item-title>
+                        </v-list-item>
+                    </router-link>
 
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-soccer</v-icon>
-                        </v-list-item-icon>
-                        
-                        <v-list-item-title>Classificação</v-list-item-title>
-                    </v-list-item>
                 </v-list-item-group>
             </v-list>
         </v-navigation-drawer>
@@ -49,5 +51,7 @@ export default {
 </script>
 
 <style scoped>
-
+    .app-top-bar a {
+        text-decoration: none;
+    }
 </style>>
